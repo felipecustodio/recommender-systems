902655d0c6b9a8058223b6e3d2db11589ad12ab8
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-07-04 22:33:12 -0300

    Add ETA for similarity matrix generation

--- assignment2/fbc_knn_reviews.py
+++ assignment2/fbc_knn_reviews.py
@@ -50,21 +50,18 @@ def similarity(movie_ids, movie_reviews, movies_data):
     tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
     tfidf_matrix = tf.fit_transform([review for index, review in enumerate(corpus)])
 
-    # calculate vector of similarities for each of the movies from the first recommender
-    # recommend the most similar for each one for the user
-
     top_n = 1
     most_similar = []
 
     print("Finding most similar movies...")
-    for movie in movie_ids:
-        cosine_similarities = linear_kernel(tfidf_matrix[movie:movie+1], tfidf_matrix).flatten()
-        related_docs_indices = [i for i in cosine_similarities.argsort()[::-1] if i != movie]
-        most_similar.append([(movie, cosine_similarities[movie]) for movie in related_docs_indices][0:top_n])
-
-    print("\n\nRecommended movies for you:")
-    # for movie in most_similar:
-    #     print("* " + movies_data['title'][movie[0][0]-1])
+    counter = 0
+    with progressbar.ProgressBar(max_value=len(movie_ids)) as bar:
+        for movie in movie_ids:
+            cosine_similarities = linear_kernel(tfidf_matrix[movie:movie+1], tfidf_matrix).flatten()
+            related_docs_indices = [i for i in cosine_similarities.argsort()[::-1] if i != movie]
+            most_similar.append([(movie, cosine_similarities[movie]) for movie in related_docs_indices][0:top_n])
+            counter += 1
+            bar.update(counter)
 
     return most_similar
 
@@ -90,13 +87,12 @@ def FBC_kNN_reviews(user, item, similarity, data):
     else:
         return (sum_a / sum_b)
 
-
 # read dataset
 movies_data = pandas.read_csv("csv/movies_data.csv")
 test_data = pandas.read_csv("csv/test_data.csv")
 reviews_data = pandas.read_csv("csv/movie_reviews.csv")
 
-write results CSV header
+# write results CSV header
 results_csv = open('results/' + algorithm + '.csv', 'w', newline='')
 results_writer = csv.writer(results_csv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 results_writer.writerow(['id', 'rating'])
